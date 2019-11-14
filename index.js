c37fe5f639380f386516fe79fb828529bdb4057a
Lovlesh Yadav <lovlesh.y@taboola.com>
2019-11-14 23:44:43 +0530

    deployement commit 1

---
+++ index.js
@@ -0,0 +1,74 @@
+// Required Libraries
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const PORT = 8998;
+const HOST = '0.0.0.0';
+
+// New dependencies
+var expressValidator = require('express-validator');
+var flash = require('express-flash');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+const expressSanitizer = require('express-sanitizer');
+
+
+// Mysql connection
+var mysql = require('mysql');
+var connection  = require('./lib/db');
+
+
+// Global paths
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var boxRouter = require('./routes/box');
+
+var app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// App session information
+app.use(expressSanitizer());
+app.use(session({ 
+	secret: '123456cat',
+	resave: false,
+	saveUninitialized: true,
+	cookie: { maxAge: 60000 }
+}))
+
+app.use(flash());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/box', boxRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+app.listen(PORT, function () {
+  console.log(`app listening on port ${PORT}`)
+})
+
+module.exports = app;
